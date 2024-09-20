c47ad98ba29a78cba076edfa4571433613f2323a
Jcpaul1226 <155691679+Jcpaul1226@users.noreply.github.com>
2024-09-19 20:07:23 -0700

    Update Two sum.c

--- Two sum.c
+++ Two sum.c
@@ -1,5 +1,14 @@
+QUESTION: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+CODE:
+
 #include <stdio.h>
 #include <stdlib.h>
+
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
